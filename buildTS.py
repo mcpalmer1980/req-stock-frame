55add3bacf0fe5e0ea517a28d3c568b8e09a0a70
phage <kyp0717@gmail.com>
2020-02-01 12:05:31 -0500

    fix build time series

--- buildTS.py
+++ buildTS.py
@@ -3,6 +3,7 @@ import asyncio
 from aiohttp import ClientSession
 import stock_info
 import pandas as pd
+import time
 
 # ask Chris why I cannot see base_url variable
 base_url = stock_info.base_url
@@ -11,7 +12,7 @@ async def reqPrice(ticker:str, session) :
     response = await session.get(base_url + ticker)
     data =  await response.json()
     price = data["chart"]["result"][0]["meta"]["regularMarketPrice"]
-    return price
+    return ticker, price
 
 async def gatherPrices(tickers:list):
     'Get price, volume, and previous close for all items in tickers list'
@@ -22,26 +23,27 @@ async def gatherPrices(tickers:list):
         results = await asyncio.gather(*tasks)
     return results
 
-def buildTimeSeries(tickers:list, timepoints = 3, delay = 5) :
+def buildTimeSeries(tickers:list, timepoints = 3, delay = 2) :
     'get data for tickers list and run time series analysis for x timepoints'
     df = pd.DataFrame(columns=[
             'timepoint',
             'ticker',
-            'price',
+            'price'
             ] )
     for tp in range(timepoints):
         results = asyncio.run(gatherPrices(tickers))
         for ticker, price in results:
-            df.append({
-                'timepoint': timepoint,
-                'ticker': ticker,
-                'price': price  })
+            df = df.append({
+                    'timepoint': tp,
+                    'ticker': ticker,
+                    'price': price  }, ignore_index=True)
 
-        df = df.append(additions)
+        # df = df.append(additions)
         time.sleep(delay)
     return df
 
 stxlist = ['AAPL','AMZN']
 res = asyncio.run(gatherPrices(stxlist))
-# tmp = buildTimeSeries(stxlist)
 print(res)
+tmp = buildTimeSeries(stxlist)
+print(tmp)
