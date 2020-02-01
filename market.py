9f0bbae8c5723ad9108e17b78e9ce915b3cc3b12
phage <kyp0717@gmail.com>
2020-02-01 11:46:09 -0500

    first commit

---
+++ market.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+import asyncio
+from aiohttp import ClientSession
+import stock_info
+import pandas as pd
+
+# ask Chris why I cannot see base_url variable
+base_url = stock_info.base_url
+async def reqPrice(ticker:str, session) :
+    'Get price, volume, and previous close for ticker'
+    response = await session.get(base_url + ticker)
+    data =  await response.json()
+    price = data["chart"]["result"][0]["meta"]["regularMarketPrice"]
+    return price
+
+async def gatherPrices(tickers:list):
+    'Get price, volume, and previous close for all items in tickers list'
+    async with ClientSession() as session:
+        tasks = []
+        for tick in tickers:
+            tasks.append(reqPrice(tick, session))
+        results = await asyncio.gather(*tasks)
+    return results
+
+def buildTimeSeries(tickers:list, timepoints = 3, delay = 5) :
+    'get data for tickers list and run time series analysis for x timepoints'
+    df = pd.DataFrame(columns=[
+            'timepoint',
+            'ticker',
+            'price',
+            ] )
+    for tp in range(timepoints):
+        results = asyncio.run(gatherPrices(tickers))
+        for ticker, price in results:
+            df.append({
+                'timepoint': timepoint,
+                'ticker': ticker,
+                'price': price  })
+
+        df = df.append(additions)
+        time.sleep(delay)
+    return df
+
+stxlist = ['AAPL','AMZN']
+res = asyncio.run(gatherPrices(stxlist))
+# tmp = buildTimeSeries(stxlist)
+print(res)
