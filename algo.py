2e2d3865c4d85c252f08fc45492d79677d88d31d
phage <kyp0717@gmail.com>
2020-02-02 18:53:26 -0500

    add trading flow

--- algo.py
+++ algo.py
@@ -16,7 +16,7 @@ def extract_lr(x):
 df3 = df.groupby('ticker').apply(extract_lr)
 print(df3)
 
-algoInventory= pd.Dataframe()
+algos= pd.Dataframe()
 
 
 def run_active_algos(mkdstat_df, position_df, algoTracker_df):
@@ -25,11 +25,20 @@ def run_active_algos(mkdstat_df, position_df, algoTracker_df):
 def run_new_algos:
     pass
 
+def reconcile(algos,positions) -> algos:
+    pass
+
+def find_opportunity(mkstatus) -> new_algo:
+    pass
 
 market_close = datetime.datetime.now().replace(hour=16, minute=0) 
 while datetime.datetime.now() < market_close:
     df = TSprice.buildTimeSeries(l)
     mktstatus= df.groupby('ticker').apply(extract_lr)
     positions = getPosition()
-    run_algos(mktstatus,positions, algoTracker)
+    reconcile(positions, algos)
+    run_algos(mktstatus,positions, algos)
+    find_opportunity(mktstatus)
+
+
 
